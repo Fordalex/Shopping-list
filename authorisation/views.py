8c3c3df8742a124d6045c85d4d51cffb113b0989
alex <alex96ford19@gmail.com>
2020-06-04 22:18:42 +0100

    Only the categories used are shown on the profile page.

--- authorisation/views.py
+++ authorisation/views.py
@@ -4,9 +4,16 @@ from items.models import Item, Category
 def profile(request):
     items = Item.objects.filter(author=request.user.username)
     category = Category.objects.filter(author=request.user.username)
+
+    # Find the categories in use 
+    categories_in_use = []
+    for item in items:
+        if item.category not in categories_in_use:
+            categories_in_use.append(item.category)
+
     content = {
         'items': items,
-        'categories': category,
+        'categories': categories_in_use,
     }
     return render(request, 'profile.html', content)
 
